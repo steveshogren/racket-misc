3a77a691efa3369834bbbe1149fdaf8c9172901a
Steve <madklowns@gmail.com>
2012-09-17 21:44:07 -0400

    finishing the clojures in chap7

--- chap7.rkt
+++ chap7.rkt
@@ -11,30 +11,23 @@
   [appC (fun : ExprC) (arg : ExprC)]
   [plusC (l : ExprC) (r : ExprC)]
   [multC (l : ExprC) (r : ExprC)]
-  [fdC (name : symbol) (arg : symbol) (body : ExprC)])
-
-(define (get-fundef n fds)
-  (cond
-    [(empty? fds) (error 'get-fundef "reference to undefined function")]
-    [(cons? fds) (cond
-                   [(symbol=? n (fdC-name (first fds))) (first fds)]
-                   [else (get-fundef n (rest fds))])]))
+  [lamC (arg : symbol) (body : ExprC)])
 
 
 (define-type Value
   [numV (n : number)]
-  [funV (name : symbol) (arg : symbol) (body : ExprC)])
+  [closV (arg : symbol) (body : ExprC) (env : Env)])
 
 (define (interp [expr : ExprC] [env : Env]) : Value
   (type-case ExprC expr
     [numC (n) (numV n)]
     [idC (n) (lookup n env)]
-    [fdC (n a b) (funV n a b)]
-    [appC (f a) (local ([define fd (interp f env)])
-              (interp (funV-body fd)
-                      (extend-env (bind (funV-arg fd)
+    [lamC (a b) (closV a b env)]
+    [appC (f a) (local ([define f-value (interp f env)])
+              (interp (closV-body f-value)
+                      (extend-env (bind (closV-arg f-value)
                                         (interp a env))
-                                  mt-env)))]
+                                  (closV-env f-value))))]
     [plusC (l r) (num+ (interp l env) (interp r env))]
     [multC (l r) (num* (interp l env) (interp r env))]))
 (define (num+ [l : Value] [r : Value]) : Value
@@ -50,7 +43,7 @@
     [else
      (error 'num+ "one argument was not a number")]))
 
-(define (lookup [for : symbol] [env : Env]) : number
+(define (lookup [for : symbol] [env : Env]) : Value
   (cond
     [(empty? env) (error 'lookup "name not found")]
     [else (cond
@@ -59,11 +52,11 @@
             [else (lookup for (rest env))])]))
 
 
-(test (interp (plusC (numC 10) (appC (fdC 'const5 '_ (numC 5)) (numC 10)))
+(test (interp (plusC (numC 10) (appC (lamC  '_ (numC 5)) (numC 10)))
               mt-env)
       (numV 15))
  
-(test/exn (interp (appC (fdC 'f1 'x (appC (fdC 'f2 'y (plusC (idC 'x) (idC 'y)))
+(test/exn (interp (appC (lamC 'x (appC (lamC  'y (plusC (idC 'x) (idC 'y)))
                                           (numC 4)))
                         (numC 3))
                   mt-env)
